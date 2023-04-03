0b7bbf74ede48723723d56ac950fea5eb9af7c06
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-04-03 14:06:48 +0300

    add prop types to statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { List, Item, Title, Stat } from "./Statistics.styled";
 
 export const Statistics = ({
@@ -32,4 +33,12 @@ export const Statistics = ({
       </Item>
     </List>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
